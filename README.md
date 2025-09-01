a10ddf04c648a872336e33485d1d5cd18006b560
Joseph Liu <drowsy.liu@gmail.com>
2025-09-01 16:44:08 +0800

    start point for this homework

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hexschool-vuejs-hw-todolist-final
+# hexschool-vuejs-hw-todolist-final-v2
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/hexschool-vuejs-hw-todolist-final/',
+  base: '/hexschool-vuejs-hw-todolist-final-v2/',
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
